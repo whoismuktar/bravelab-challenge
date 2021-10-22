66a5e8a8d8e9247b422a3f11ab372584db4d1fa5
Muktar <mayunited12@yahoo.com>
2021-10-22 08:02:56 +0100

    init base setup completed

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    <LoginCard />
+  </div>
+</template>
+
+<script>
+import LoginCard from './components/LoginCard'
+
+export default {
+  name: 'Login',
+  components: { LoginCard },
+}
+</script>
