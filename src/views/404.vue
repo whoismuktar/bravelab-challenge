1367d833835840f745c3221bca392b4b5d214084
Muktar <mayunited12@yahoo.com>
2021-10-24 02:17:29 +0100

    implement fallback 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page 404 notfound-page">
+    <div
+      class="notfound__container allChildrenCenter fillHeight"
+    >
+      <h2 class="text-center">
+          <b>404</b>
+        </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.notfound__container {
+    overflow: hidden;
+}
+</style>
