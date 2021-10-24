5a3f995f95e9e547fb6bb80ee6eca6fcb7badfee
Muktar <mayunited12@yahoo.com>
2021-10-24 02:11:54 +0100

    main base js with nav guard round up

--- src/main.js
+++ src/main.js
@@ -2,8 +2,47 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import toast from './components/toastMsg'
+// base scss
+import "./assets/styles/base.scss"
 
 Vue.config.productionTip = false
+
+// inject re-useabel global toast
+Vue.component('toast', toast)
+
+// ( %Y-%m-%d ) date formatter filter
+Vue.filter('ymd', function (value) {
+  return `${new Date(value).toISOString().slice(0, 10)}`;
+});
+
+// Navigation Auth Guard
+router.beforeEach(async (to, from, next) => {
+  const status = store.getters['auth/isSignedIn']
+
+  // Guard for Authenticated routes
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (status) {
+      next();
+    } else {
+      next({
+        path: "/",
+      });
+    }
+
+  } else if (to.matched.some((record) => record.meta.requiresVisitor)) {
+    // Guard for Non Authenticated routes
+    if (!status) {
+      next();
+    } else {
+      next({
+        path: "/dashboard",
+      });
+    }
+  } else {
+    next();
+  }
+});
 
 new Vue({
   store,
